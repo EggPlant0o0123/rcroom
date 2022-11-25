4c1d8f82abc14f9dcebbe461b675389dba20ff4e
EggPlant0o0123 <joelchongxj@gmail.com>
2022-11-25 23:22:22 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/rcroom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rcroom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(rcRoom());
+}
+
+class rcRoom extends StatelessWidget {
+  const rcRoom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
